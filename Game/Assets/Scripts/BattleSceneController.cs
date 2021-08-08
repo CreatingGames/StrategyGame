9ad1f4afd758253af4845ddb8f2b2d30d732496d
bigstone04 <m.bigstone.hailab@gmail.com>
2021-08-08 17:56:11 +0900

    コントローラーの追加

---
+++ Game/Assets/Scripts/BattleSceneController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSceneController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
