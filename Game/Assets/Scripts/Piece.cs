1da1a2a11ce7d92b0977d63de68883f77a2c5dc4
bigstone04 <m.bigstone.hailab@gmail.com>
2021-08-08 17:56:40 +0900

    テスト用の変数を追加 初期化メソッドの作成

--- Game/Assets/Scripts/Piece.cs
+++ Game/Assets/Scripts/Piece.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
+    // Test用データ入力
+    [SerializeField] int t_UpperLeft = 0;
+    [SerializeField] int t_LowerLeft = 0;
+    [SerializeField] int t_UpperRight = 0;
+    [SerializeField] int t_LowerRight = 0;
+    [SerializeField] int t_Left = 0;
+    [SerializeField] int t_Right = 0;
+    [SerializeField] int t_Forward = 0;
+    [SerializeField] int t_Backward = 0;
+
     // 駒の行動範囲
     public int UpperLeft { get; set; } = 0;// 左斜め前
     public int LowerLeft { get; set; } = 0;// 左斜め後ろ
@@ -16,6 +26,22 @@ public class Piece : MonoBehaviour
 
     // 駒の状態
     public bool evolved { get; set; } = false;
+    private void Start()
+    {
+        InitActionRange(t_UpperLeft, t_LowerLeft, t_UpperRight, t_LowerRight, t_Left, t_Right, t_Forward, t_Backward);
+    }
+
+    public void InitActionRange(int UpperLeft, int LowerLeft, int UpperRight, int LowerRight, int Left, int Right, int Forward, int Backward)
+    {
+        this.UpperLeft = UpperLeft;
+        this.LowerLeft = LowerLeft;
+        this.UpperRight = UpperRight;
+        this.LowerRight = LowerRight;
+        this.Left = Left;
+        this.Right = Right;
+        this.Forward = Forward;
+        this.Backward = Backward;
+    }
 
     // 駒が保有する戦略ポイントを返す。
     public int GetStrategyPoint()
