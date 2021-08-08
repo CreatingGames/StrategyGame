e9007b15f0740534826dc6bfedd7aea99550b842
bigstone04 <m.bigstone.hailab@gmail.com>
2021-08-08 21:36:23 +0900

    盤面の作成、升目の透明度変化メソッドの作成

--- Game/Assets/Scripts/BattleSceneController.cs
+++ Game/Assets/Scripts/BattleSceneController.cs
@@ -1,16 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleSceneController : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject ParentBoard;
+    // Test
+    [SerializeField] int x;
+    [SerializeField] int y;
 
-    void Update()
+    private GameObject[,] ChildBoard;
+    private void Start()
     {
-        
+        GetAllChildBoard();
+        ChangeImageTransparency(x, y);
+    }
+    // インスペクターで取得したBoardから子要素のそれぞれのImageを取得する
+    private void GetAllChildBoard()
+    {
+        int boardSize;
+        if (ParentBoard.transform.childCount == 5 * 5)
+        {
+            boardSize = 5;
+        }
+        else if (ParentBoard.transform.childCount == 6 * 6)
+        {
+            boardSize = 6;
+        }
+        else
+        {
+            boardSize = 7;
+        }
+        ChildBoard = new GameObject[boardSize, boardSize];
+        int index = 0;
+        for (int i = 0; i < boardSize; i++)
+        {
+            for (int j = 0; j < boardSize; j++)
+            {
+                ChildBoard[i, j] = ParentBoard.transform.GetChild(index).gameObject;
+                index++;
+            }
+        }
+    }
+    public void ChangeImageTransparency(int x,int y)
+    {
+        float red = ChildBoard[y, x].GetComponent<Image>().color.r;
+        float green = ChildBoard[y, x].GetComponent<Image>().color.g;
+        float blue = ChildBoard[y, x].GetComponent<Image>().color.b;
+        float alfa = 255;
+        ChildBoard[y, x].GetComponent<Image>().color = new Color(red, green, blue, alfa);
     }
 }
