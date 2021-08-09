a7dcb70da59345432f338e2e4e9a431ceeaffb24
bigstone04 <m.bigstone.hailab@gmail.com>
2021-08-09 15:19:09 +0900

    Pieceデータクラスの実装

--- Game/Assets/Scripts/FormationData.cs
+++ Game/Assets/Scripts/FormationData.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class FormationData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public struct Formation
     {
-        
+        public int UpperLeft;// 左斜め前
+        public int LowerLeft;// 左斜め後ろ
+        public int UpperRight;// 右斜め前
+        public int LowerRight;// 右斜め後ろ
+        public int Left;// 左
+        public int Right;// 右
+        public int Forward;// 前
+        public int Backward;// 後ろ
     }
-
-    // Update is called once per frame
-    void Update()
+    public Formation[,] shortBoard = new Formation[2, 5];
+    private void Start()
     {
-        
+
     }
 }
--- Game/Assets/Scripts/Piece.cs
+++ Game/Assets/Scripts/Piece.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Piece : MonoBehaviour
+public class Piece : PieceData
 {
     // Test用データ入力
     [SerializeField] int t_UpperLeft = 0;
@@ -14,15 +14,7 @@ public class Piece : MonoBehaviour
     [SerializeField] int t_Forward = 0;
     [SerializeField] int t_Backward = 0;
 
-    // 駒の行動範囲
-    public int UpperLeft { get; set; } = 0;// 左斜め前
-    public int LowerLeft { get; set; } = 0;// 左斜め後ろ
-    public int UpperRight { get; set; } = 0;// 右斜め前
-    public int LowerRight { get; set; } = 0;// 右斜め後ろ
-    public int Left { get; set; } = 0;// 左
-    public int Right { get; set; } = 0;// 右
-    public int Forward { get; set; } = 0;// 前
-    public int Backward { get; set; } = 0;// 後ろ
+
 
     // 駒の状態
     public bool evolved { get; set; } = false;
