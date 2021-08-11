43802af3517fc30eab3a79a084f58f7b228c8935
bigstone04 <m.bigstone.hailab@gmail.com>
2021-08-11 22:43:27 +0900

    テスト用に駒の行動範囲を追加

--- Game/Assets/Scripts/FormationData.cs
+++ Game/Assets/Scripts/FormationData.cs
@@ -10,6 +10,13 @@ public class FormationData : MonoBehaviour
         shortBoard = new PieceData[2, 5];
         shortBoard[1, 2] = gameObject.AddComponent<PieceData>();
         shortBoard[1, 2].Forward = 1;
+        shortBoard[1, 2].Backward = 1;
+        shortBoard[1, 2].Right = 1;
+        shortBoard[1, 2].Left = 1;
+        shortBoard[1, 2].UpperLeft = 1;
+        shortBoard[1, 2].LowerLeft = 1;
+        shortBoard[1, 2].UpperRight = 1;
+        shortBoard[1, 2].LowerLeft = 1;
         isComplete = true;
 
     }
