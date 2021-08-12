7ef426dab5b31849632fd03102e038433277c780
bigstone04 <m.bigstone.hailab@gmail.com>
2021-08-12 20:51:00 +0900

    相手陣地を設定

--- Game/Assets/Scripts/FormationData.cs
+++ Game/Assets/Scripts/FormationData.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 public class FormationData : MonoBehaviour
 {
     public PieceData[,] MyFormationBoard;
-    public bool isComplete = false;
-    public void InitFormationData()
+    public PieceData[,] OpponentFormationBoard;
+    public bool InitializedMyformation = false;
+    public bool InitializedOpponentformation = false;
+    public void InitMyFormationData()
     {
         MyFormationBoard = new PieceData[2, 5];
         MyFormationBoard[0, 2] = gameObject.AddComponent<PieceData>();
@@ -17,8 +19,21 @@ public class FormationData : MonoBehaviour
         MyFormationBoard[0, 2].LowerLeft = 3;
         MyFormationBoard[0, 2].UpperRight = 3;
         MyFormationBoard[0, 2].LowerRight = 3;
-        isComplete = true;
+        InitializedMyformation = true;
 
     }
-
+    public void InitOpponentFormationData()
+    {
+        OpponentFormationBoard = new PieceData[2, 5];
+        OpponentFormationBoard[0, 2] = gameObject.AddComponent<PieceData>();
+        OpponentFormationBoard[0, 2].Forward = 0;
+        OpponentFormationBoard[0, 2].Backward = 0;
+        OpponentFormationBoard[0, 2].Right = 0;
+        OpponentFormationBoard[0, 2].Left = 0;
+        OpponentFormationBoard[0, 2].UpperLeft = 3;
+        OpponentFormationBoard[0, 2].LowerLeft = 0;
+        OpponentFormationBoard[0, 2].UpperRight = 3;
+        OpponentFormationBoard[0, 2].LowerRight = 0;
+        InitializedOpponentformation = true;
+    }
 }
