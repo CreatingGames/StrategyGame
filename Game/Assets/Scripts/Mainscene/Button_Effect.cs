696c3d87270b867a7d668891203777639f8bc8d3
kaname-san <kanameosu@gmail.com>
2021-09-05 23:13:11 +0900

    マウスが重なったら筆のエフェクトが出るようにした＋フォントを追加した

---
+++ Game/Assets/Scripts/Mainscene/Button_Effect.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Button_Effect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+
+{
+    //public Image image;
+    public Animator animator;
+
+
+    // オブジェクトの範囲内にマウスポインタが入った際に呼び出されます。
+    // this method called by mouse-pointer enter the object.
+   public void OnPointerEnter(PointerEventData eventData)
+   {
+      Debug.Log("true");
+      animator.SetBool("hude_effect", true);
+    }
+
+   public void OnPointerExit(PointerEventData eventData)
+   {
+       Debug.Log("false");
+        //animator.SetFloat(Animator.StringToHash("speed"), -1);
+        animator.SetBool("hude_effect", false);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
